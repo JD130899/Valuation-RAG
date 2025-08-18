66d19c840a6b7505160bbfcd2310007926d67b25
JD130899 <167375498+JD130899@users.noreply.github.com>
2025-08-18 16:48:23 -0400

    Update Streamlit4.py

--- Streamlit4.py
+++ Streamlit4.py
@@ -3,7 +3,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(page_title="Pinned Buttons Chat", layout="wide")
-st.markdown("<style>.block-container{padding-bottom:160px!important}</style>", unsafe_allow_html=True)
+
 
 # ---------------- state ----------------
 if "messages" not in st.session_state:
